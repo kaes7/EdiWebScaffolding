6f822a69b3500a952b54defbe437d15c55533b28
ksc77 <kiemonik1993@o2.pl>
2018-10-23 14:12:42 +0200

    Change Home Controller

--- ComarchEdiWebScaffolding/Controllers/HomeController.cs
+++ ComarchEdiWebScaffolding/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace ComarchEdiWebScaffolding.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page. Test Commit";
 
             return View();
         }
