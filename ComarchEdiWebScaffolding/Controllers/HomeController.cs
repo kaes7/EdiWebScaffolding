ce4810608f92dfba264d9f5ea35cb4679bf99a16
ksc77 <kiemonik1993@o2.pl>
2018-10-23 14:19:23 +0200

    Test

--- ComarchEdiWebScaffolding/Controllers/HomeController.cs
+++ ComarchEdiWebScaffolding/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace ComarchEdiWebScaffolding.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page. Test Commit s";
+            ViewData["Message"] = "Your contact page. Test Commit s cv";
 
             return View();
         }
