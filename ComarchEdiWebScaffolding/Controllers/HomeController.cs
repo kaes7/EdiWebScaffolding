0838b5d61a3ed87c5ad884700579534867ef1fff
ksc77 <kiemonik1993@o2.pl>
2018-10-23 14:17:11 +0200

    remove Client

--- ComarchEdiWebScaffolding/Controllers/HomeController.cs
+++ ComarchEdiWebScaffolding/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace ComarchEdiWebScaffolding.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page. Test Commit";
+            ViewData["Message"] = "Your contact page. Test Commit s";
 
             return View();
         }
